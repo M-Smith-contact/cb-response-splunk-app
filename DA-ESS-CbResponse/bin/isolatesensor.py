cf0da21eea906fbc6612f93a069b120656cf20a1
Jason Garman <jgarman@bit9.com>
2016-09-19 12:49:52 -0400

    merge changes from Kyle Champlin to fix compat with new Adaptive Response API

--- DA-ESS-CbResponse/bin/isolatesensor.py
+++ DA-ESS-CbResponse/bin/isolatesensor.py
@@ -15,14 +15,12 @@ sys.path.append(make_splunkhome_path(["etc", "apps", "SA-Utils", "lib"]))
 
 from cim_actions import ModularAction
 from splunklib.client import Service
-from cbhelpers import get_cbapi, setup_logger
+from cbhelpers import get_cbapi
 from cbapi.response.models import Sensor
 from time import sleep
 
 import re
 
-
-logger = setup_logger()
 
 IPv4_re = re.compile("(\d+)\.(\d+)\.(\d+)\.(\d+)")
 
@@ -74,10 +72,12 @@ class IsolateSensorAction(ModularAction):
                 retries_left -= 1
 
             if sensor.is_isolating:
-                logger.info("Sensor id {0} successfully isolated!".format(sensor_id))
+                self.addevent("Sensor id {0} successfully isolated".format(sensor_id),
+                              sourcetype="bit9:carbonblack:action")
                 return True
             else:
-                logger.info("Sensor id {0} not isolated; retries exceeded".format(sensor_id))
+                self.addevent("Sensor id {0} not isolated; retries exceeded".format(sensor_id),
+                              sourcetype="bit9:carbonblack:action")
                 return False
 
     def do_cbevent(self, cb, result):
@@ -132,10 +132,14 @@ class IsolateSensorAction(ModularAction):
             # variety of reasons.
             logger.info("Looking for a sensor with {0} of {1}...".format(field_type, ip_or_hostname))
             sensor_id = cb.select(Sensor).where("{0}:{1}".format(field_type, ip_or_hostname)).first().id
-            logger.info("Found a sensor with a {0} of {1}: Calling do_isolate with sensor id {2}."
-                        .format(field_type, ip_or_hostname, sensor_id))
+            logger.info("Found a sensor with a {0} of {1}: Calling do_isolate with sensor id {2}.".format(field_type,
+                                                                                                          ip_or_hostname,
+                                                                                                          sensor_id))
             return self.do_isolate(cb, sensor_id)
         except Exception as e:
+            # modaction.message('Failed to create splunk event: ' + str(e), status='failure',rids=modaction.rids,level=logging.ERROR)
+
+            modaction.message('RESULT: ' + str(result), level=logging.ERROR)
             self.error("Could not isolate sensor: {0}".format(str(e)))
             logger.exception("Detailed error message")
 
@@ -148,6 +152,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     try:
+        logger = ModularAction.setup_logger('isolate_modalert')
         logger.info("Calling IsolateSensorAction.__init__")
         modaction = IsolateSensorAction(sys.stdin.read(), logger, 'isolatesensor')
         logger.info("Returned IsolateSensorAction.__init__")
@@ -157,19 +162,24 @@ if __name__ == "__main__":
 
     try:
         session_key = modaction.session_key
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug('%s', json.dumps(modaction.settings, sort_keys=True,
-                indent=4, separators=(',', ': ')))
-
         ## process results
+        modaction.addinfo()
         with gzip.open(modaction.results_file, 'rb') as fh:
             for num, result in enumerate(csv.DictReader(fh)):
                 ## set rid to row # (0->n) if unset
-                result.setdefault('rid', num)
+                result.setdefault('rid', str(num))
                 modaction.update(result)
                 modaction.invoke()
-                modaction.dowork(result)
-                modaction.writeevents(index='main', source='bit9:carbonblack')
+                act_result = modaction.dowork(result)
+                modaction.addevent(str(act_result), sourcetype="bit9:carbonblack:action")
+
+                if act_result:
+                    modaction.writeevents(index='main', source='carbonblackapi')
+                    modaction.message('Successfully created splunk event', status='success', rids=modaction.rids)
+                else:
+                    modaction.writeevents(index='main', source='carbonblackapi')
+                    modaction.message('Failed to create splunk event', status='failure', rids=modaction.rids,
+                                      level=logging.ERROR)
 
     except Exception as e:
         ## adding additional logging since adhoc search invocations do not write to stderr
